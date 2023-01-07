217d6d3550af591cc068043866c330adca28a6eb
Tharusan Vijayakumar <tharusan.vijayakumar@gmail.com>
2023-01-07 18:15:15 +0000

    End Screen

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -6,19 +6,28 @@ public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
     public float flapSpeed;
+    public MetaScript meta;
+    public bool birdAlive = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        meta = GameObject.FindGameObjectWithTag("MetaLogic").GetComponent<MetaScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) == true)
+        if(Input.GetKeyDown(KeyCode.Space) == true && birdAlive)
         {
             //Vector2 refers to (X,Y) and .up means (0,1) 
             myRigidbody.velocity = Vector2.up * flapSpeed;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        meta.gameOver();
+        birdAlive = false;
     }
 }
--- Assets/MetaScript.cs
+++ Assets/MetaScript.cs
@@ -2,16 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MetaScript : MonoBehaviour
 {
     public int playerScore;
     public Text scoreText;
+    public GameObject gameOverScreen;
 
     [ContextMenu("Increase Score")] //This allows us to run the function from Unity useful for testing
     public void addScore(int scoreToAdd)
     {
         playerScore = playerScore + scoreToAdd;
         scoreText.text = playerScore.ToString();
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
     }
 }
